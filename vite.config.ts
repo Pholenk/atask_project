5e58eab29792de9505bfa4990146839c0efb4d6b
pholenk <pholenkadi17@gmail.com>
2025-07-16 22:21:06 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/atask_project/atask_project/',
-  plugins: [react(), tailwindcss(),],
+  base: '/atask_project/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -22,4 +22,4 @@ export default defineConfig({
       '@slices': path.resolve(__dirname, 'src/store/slices'),
     },
   },
-})
+});
